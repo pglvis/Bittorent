1cda8ef8cb78a91700771d1252272b62c1571151
Vishal Sharma <vishalsharma331998@gmail.com>
2021-08-15 23:22:51 +0530

    connection request and response functions

--- Torrent/tracker.js
+++ Torrent/tracker.js
@@ -3,6 +3,7 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+const crypto = require('crypto');
 
 module.exports.getPeers = (torrent , callback) => {
     const socket = dgram.createSocket('udp4');
@@ -27,3 +28,45 @@ module.exports.getPeers = (torrent , callback) => {
         }
     });
 };
+
+function udpSend(socket , message , rawUrl , callback=()=> {}){
+    const url = urlParse(rawUrl);
+    socket.send(message , 0 , message.length , url.port , url.host , callback);
+}
+
+function respType(response){
+
+}
+
+function buildConnReq(){
+    const buf = Buffer.alloc(16);
+
+    // connection id
+    buf.writeUInt32BE(0x417 , 0);
+    buf.writeUInt32BE(0x27101980 , 4);
+
+    // action
+    buf.writeUInt32BE(0,8)
+
+    // transaction id
+    crypto.randomBytes(4).copy(buf , 12);
+
+    return buf;
+
+}
+
+function parseConnResp(resp){
+    return{
+        action: resp.readUInt32BE(0),
+        transactionId: resp.readUInt32BE(4),
+        connectionId: resp.slice(8)
+    }
+}
+
+function buidlAnnounceReq(connId){
+
+}
+
+function parseAnnounceResp(resp){
+
+}
