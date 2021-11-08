696aac8431ad904f4867ca8d58a3c86030e321e1
Vishal Sharma <vishalsharma331998@gmail.com>
2021-11-08 09:39:28 +0530

    restructured the files

--- src/tracker.js
+++ src/tracker.js
@@ -37,7 +37,9 @@ function udpSend(socket , message , rawUrl , callback=()=> {}){
 }
 
 function respType(response){
-
+    const action = response.readUInt32BE(0);
+    if(action === 0) return 'connect';
+    if(action === 1) return 'announce';
 }
 
 function buildConnReq(){
